4c21a348e94cb7390e047b84fe7c71432c7c0b77
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-19 17:17:28 +0100

    Changement pour une boutique de voiture.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`,
+  template: `<h1>Welcome to {{ vehicleList[0] }}!</h1>`,
   styles: []
 })
-export class AppComponent {
-  title = 'boutique de textures';
+export class AppComponent implements OnInit {
+  vehicleList = ['Twizy E-Tech 100% Electric', 'Twingo', 'Zoe E-Tech 100% Electric'];
+
+  ngOnInit(): void {
+    console.table(this.vehicleList);
+    this.selectTexture('TWINGO');
+  }
+
+  /**
+   * Permet de sélectionner un asset.
+   * @param vehicleName Nom de l'asset.
+   */
+  selectTexture(vehicleName: string) {
+    console.log(`Vous avez cliqué sur le pokémon ${vehicleName}`);
+  }
 }

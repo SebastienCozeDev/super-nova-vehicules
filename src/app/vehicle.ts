0066af0ff217663ec9246b6056ae6ac1643d3997
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-19 17:26:25 +0100

    Ajout du model Vehicle pour les véhicules.

---
+++ src/app/vehicle.ts
@@ -0,0 +1,10 @@
+export class Vehicle {
+    id: number;
+    price: number;
+    name: string;
+    description: string;
+    picture: string;
+    energyClass: string;
+    types: Array<string>;
+    release: Date;
+}

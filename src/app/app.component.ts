78e294a5d4017eb8d235223909d57b6afd118baf
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-19 20:41:41 +0100

    Modification du root component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,23 @@ import { VEHICLES } from './mock-vehicle-list';
 })
 export class AppComponent implements OnInit {
   vehicleList: Vehicle[] = VEHICLES;
+  vehicleSelected: Vehicle|undefined;
 
   ngOnInit(): void {
     console.table(this.vehicleList);
-    this.selectVehicle(this.vehicleList[1]);
   }
 
   /**
    * Permet de sélectionner un véhicule.
    * @param vehicle Véhicule.
    */
-  selectVehicle(vehicle: Vehicle) {
-    console.log(`Vous avez cliqué sur le véhicule ${vehicle.name}`);
+  selectVehicle(vehicleId: String) {
+    const vehicle: Vehicle|undefined = this.vehicleList.find(vehicle => vehicle.id == +vehicleId);
+    if (vehicle) {
+      console.log(`Vous avez cliqué sur le véhicule ${vehicle.name}`);
+    } else {
+      console.log(`Vous avez demandé un véhicule qui n'existe pas.`);
+    }
+    this.vehicleSelected = vehicle;
   }
 }

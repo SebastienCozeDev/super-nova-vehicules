90be85bfc5a74ccc3c6ff34b5f2e4656dff0aba2
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-23 21:20:25 +0100

    Création des composant detail-vehicle et list-vehicles.

---
+++ src/app/detail-vehicle/detail-vehicle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-vehicle',
+  templateUrl: './detail-vehicle.component.html',
+  styles: [
+  ]
+})
+export class DetailVehicleComponent {
+
+}
---
+++ src/app/list-vehicles/list-vehicles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-vehicles',
+  templateUrl: './list-vehicles.component.html',
+  styles: [
+  ]
+})
+export class ListVehiclesComponent {
+
+}

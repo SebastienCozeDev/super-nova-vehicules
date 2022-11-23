f269acb5ff03b3d194cc6268e6547f79c4b9445b
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-23 21:28:22 +0100

    Création des routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailVehicleComponent } from './detail-vehicle/detail-vehicle.component';
+import { ListVehiclesComponent } from './list-vehicles/list-vehicles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'vehicles', component: ListVehiclesComponent },
+  { path: 'vehicle/:id', component: DetailVehicleComponent },
+  { path: '', redirectTo: 'vehicles', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

51c1d1a64ef52bd5d58fbc2330401665fefe4705
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-23 22:41:15 +0100

    Gestion de l'erreur 404.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailVehicleComponent } from './detail-vehicle/detail-vehicle.component';
 import { ListVehiclesComponent } from './list-vehicles/list-vehicles.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'vehicles', component: ListVehiclesComponent },
   { path: 'vehicle/:id', component: DetailVehicleComponent },
-  { path: '', redirectTo: 'vehicles', pathMatch: 'full' }
+  { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `
-    <p>
-      page-not-found works!
-    </p>
+    <div class="center">
+      <h2 class="title" style="text-align: center; color: #000; text-transform: uppercase; font-size: 1.7em; margin-top: 4rem; margin-bottom: 4rem;">Oups ! Cette page n'existe pas</h2>
+      <a routerLink="/vehicles" class="waves-effect waves-teal btn-flat" style="margin-top: 4rem; margin-bottom: 4rem;">
+        Retourner à l'accueil
+      </a>
+    </div>
   `,
   styles: [
   ]

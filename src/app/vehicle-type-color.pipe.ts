cf8975ea67324dd83348b77f9ef3409b20977f49
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-23 12:40:13 +0100

    Ajout de la méthode transform.

--- src/app/vehicle-type-color.pipe.ts
+++ src/app/vehicle-type-color.pipe.ts
@@ -5,8 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class VehicleTypeColorPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(type: string): string {
+    let color: string;
+    switch (type) {
+      case 'full hybrid':
+        color = 'blue lighten-1';
+        break;
+      case 'electric':
+        color = 'blue lighten-1';
+        break;
+      case 'essence':
+        color = 'deep-orange';
+        break;
+      case 'diesel':
+        color = 'deep-purple darken-2';
+        break;
+      default:
+        color = 'grey';
+        break;
+    }
+    return 'chip ' + color;
   }
 
 }

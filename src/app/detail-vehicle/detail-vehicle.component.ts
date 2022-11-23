1957cf29bb2abb36cfc2dc85a1c15abaefe978b3
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2022-11-23 21:53:48 +0100

    Ajout de la partie logique du composant detail-vehicle.

--- src/app/detail-vehicle/detail-vehicle.component.ts
+++ src/app/detail-vehicle/detail-vehicle.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { VEHICLES } from '../mock-vehicle-list';
+import { Vehicle } from '../vehicle';
 
 @Component({
   selector: 'app-detail-vehicle',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class DetailVehicleComponent {
+export class DetailVehicleComponent implements OnInit {
+
+
+  /**
+   * Liste des véhicules.
+   */
+  vehicleList: Vehicle[];
+
+  /**
+   * Véhicule choisi par l'utilisateur.
+   */
+  vehicle: Vehicle|undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  /**
+   * Se lance à l'initialisation du composant.
+   */
+  ngOnInit(): void {
+    this.vehicleList = VEHICLES;
+    const vehicleId: string|null = this.route.snapshot.paramMap.get('id');
+    if (vehicleId) {
+      this.vehicle = this.vehicleList.find(vehicle => vehicle.id == +vehicleId);
+    }
+  }
 
 }
